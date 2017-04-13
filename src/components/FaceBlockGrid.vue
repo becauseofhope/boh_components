2a613a5ae90092c6165231a9dd795b37a5e0468d
Nelson Chen <crazysim@gmail.com>
2017-04-13 01:42:47 -0700

    Include foundation and use SCSS workaround.

--- src/components/FaceBlockGrid.vue
+++ src/components/FaceBlockGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Face Grid Here
+    Face Grid Here!
   </div>
 </template>
 
@@ -10,5 +10,6 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+  @import "face-block-grid";
 </style>
