071f0012c4cc370d21690479f7222fd7bc870b7a
Nelson Chen <crazysim@gmail.com>
2017-05-07 11:53:48 -0700

    prototype grid

--- src/components/FaceBlockGrid.vue
+++ src/components/FaceBlockGrid.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-    Face Grid Here!
-    <img src="../assets/bssp/bagume.jpg" alt="">
     <div class="row small-up-2 medium-up-3 large-up-4">
       <div class="column column-block">
-        <img src="//placehold.it/600x600" class="thumbnail" alt="">
+        <img src="//placehold.it/300x300" class="thumbnail" alt="">
       </div>
       <div class="column column-block">
-        <img src="//placehold.it/600x600" class="thumbnail" alt="">
+        <img src="//placehold.it/300x300" class="thumbnail" alt="">
       </div>
       <div class="column column-block">
-        <img src="//placehold.it/600x600" class="thumbnail" alt="">
+        <img src="//placehold.it/300x300" class="thumbnail" alt="">
       </div>
       <div class="column column-block">
-        <img src="//placehold.it/600x600" class="thumbnail" alt="">
+        <img src="//placehold.it/300x300" class="thumbnail" alt="">
       </div>
       <div class="column column-block">
-        <img src="//placehold.it/600x600" class="thumbnail" alt="">
+        <img src="//placehold.it/300x300" class="thumbnail" alt="">
       </div>
       <div class="column column-block">
-        <img src="//placehold.it/600x600" class="thumbnail" alt="">
+        <img src="//placehold.it/300x300" class="thumbnail" alt="">
       </div>
     </div>
   </div>
