f14bc744745e6a1d73ec8fb408fd1446c95e218f
Nelson Chen <crazysim@gmail.com>
2017-10-20 01:01:08 -0700

    Initial hover implemented

--- src/components/face_script.ts
+++ src/components/face_script.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Face as FaceData } from '../data/face';
+import * as textFit from 'textFit';
 
 @Component({
   props: {
@@ -11,4 +12,8 @@ import { Face as FaceData } from '../data/face';
 export default class Face extends Vue {
   face: FaceData;
   popupActive = false;
+  mounted () {
+    console.log("Ready");
+    textFit(this.$el.getElementsByClassName("description"));
+  }
 };
