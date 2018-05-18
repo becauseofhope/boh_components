7bea94221cd5ddcb60763997c57d0da4641bc920
Nelson Chen <crazysim@gmail.com>
2018-05-17 19:48:12 -0700

    Cleanup and split images for graduated

--- src/data/graduated_bssp.ts
+++ src/data/graduated_bssp.ts
@@ -4,17 +4,17 @@ export const Faces: Face[] = [
   {
     "title": "Christopher",
     "description": "Christopher graduated in January of 2018 from Makerere University. He received his degree in Developmental Studies. Upon graduating, he offered to volunteer with BOH in hopes of using his knowledge to build up the community he was raised in. Now he’s employed with BOH to help WOCAP start earning income.",
-    "imageName": require("../assets/images/bssp/Christopher.jpg")
+    "imageName": require("../assets/images/graduated_bssp/Christopher.jpg")
   },
   {
     "title": "Martha",
     "description": "Martha attended Makerere University and has her BA in Teaching. Before her graduation in 2018, she had already attained a teaching position. She teaches History and Religious Education at an all girls secondary school.",
-    "imageName": require("../assets/images/bssp/Martha2016.jpg")
+    "imageName": require("../assets/images/graduated_bssp/Martha2016.jpg")
   },
   {
     "title": "Tom",
     "description": "Tom is 27 years old and hopes to someday be a chef. He is in a catering program working hard toward this goal. He is dedicated to his family and cares deeply for his parents and siblings.",
-    "imageName": require("../assets/images/bssp/Tom2016.jpg")
+    "imageName": require("../assets/images/graduated_bssp/Tom2016.jpg")
   },
 ];
 
