94bb9e76b925983b350056f3d74999fa8612f250
Nelson Chen <crazysim@gmail.com>
2020-09-01 21:03:44 -0700

    Updated text

--- src/data/bssp.ts
+++ src/data/bssp.ts
@@ -3,52 +3,52 @@ import Face from './face';
 export const Faces: Face[] = [
   {
     "title": "Agnes M.",
-    "description": "The youngest of 7 kids, Agnes is 11 years old and lives in Nakakulwe village. She loves school, particularly writing, and she wants to be a nurse. She also enjoys skipping and her favorite food is rice and meat.",
+    "description": "The youngest of 7 kids, Agnes is 13 years old and is from Nakakulwe village. She is in her first year of secondary school and loves it. She particularly enjoys writing and she wants to be a nurse. She also enjoys skipping and her favorite food is rice and meat.",
     "imageName": require("../assets/images/bssp/Agnes2018.jpg")
   },
   {
     "title": "Agnes N.",
-    "description": "Agnes is sixteen years old and the youngest in her family. She is from Nakakulwe village and taken care of by her mother Catherine who encourages Agnes to study hard. Agnes is in her second year of secondary school.",
+    "description": "Agnes is 18 years old and the youngest in her family. She is from Nakakulwe village and is taken care of by her mother Catherine, who encourages Agnes to study hard. Agnes is in her first year of vocational school, pursuing a certificate in accounting.",
     "imageName": require("../assets/images/bssp/Agnes_Nuume2018.jpg")
   },
   {
     "title": "Azizi",
-    "description": "Azizi is 13 years old and taken care of by his aunt. He likes English and wants to be a doctor. He is all smiles and laughter, and when he plays soccer you can hear him laughing a mile away. His favorite foods are rice, chicken, and pasta.",
+    "description": "Azizi is 15 years old and taken care of by his aunt. He likes English and wants to be a doctor. He is all smiles and laughter, and when he plays soccer you can hear him laughing a mile away. His favorite foods are rice, chicken, and pasta.",
     "imageName": require("../assets/images/bssp/Aziz2018.jpg")
   },
   {
     "title": "Christine N.",
-    "description": "Christine is an ambitious 25-year-old who hopes to someday be a successful business woman. She is in a business administration program working hard to achieve her dreams.",
+    "description": "Christine is an ambitious 26-year-old who hopes to someday be a successful business woman. She is in a business administration program, working hard to achieve her dreams.",
     "imageName": require("../assets/images/bssp/ChristineN.jpg")
   },
   {
     "title": "Cyprian",
-    "description": "Cyprian is from Namagera village, where his grandmother still lives. He finished a vocational program in Motor Vehicle Mechanics. Currently, he’s taking a one year advanced certificate program which will allow him to apply for university. Cyprian is extremely polite and kind, and he sets a good example for younger students in BSSP.",
+    "description": "Cyprian is from Namagera village, where his grandmother still lives. He is pursuing a degree in Motor Vehicle Engineering at Kampala International University. Cyprian is extremely polite and kind, and he sets a good example for younger students in BSSP.",
     "imageName": require("../assets/images/bssp/Cyprian2018.jpg")
   },
   {
     "title": "Derick",
-    "description": "Derick is the eldest of nine children, who all live in Bubugo village with their mother, Ruth. Derick is very hard working and is excited to be in a program for motor vehicle engineering.",
+    "description": "Derick is the eldest of nine children, who all live in Bubugo village with their mother, Ruth. Derick is very hard working and has graduated from vocational school with a degree in motor vehicle engineering. He is now in his second year at Kampala International University.",
     "imageName": require("../assets/images/bssp/Derick2018.jpg")
   },
   {
     "title": "Gerald",
-    "description": "Gerald is a 19 year old vocational student who hopes to one day be an engineer. He works hard in school to “fight for his future.” He is a leader at school and his best subject is English. Gerald enjoys playing soccer and his favorite food is rice and chicken.",
+    "description": "Gerald is a 22-year-old university student who hopes to one day be an engineer. He works hard in school to “fight for his future.” He is a leader at school and his best subject is English. Gerald enjoys playing soccer and his favorite food is rice and chicken.",
     "imageName": require("../assets/images/bssp/Gerald2018.jpg")
   },
   {
     "title": "Imai",
-    "description": "Fourteen-year-old Imai is a secondary student this year from Bubugo village. She thrives at school and is quick to show compassion to all those around her. Imai loves science and dreams of finishing school so she can become a nun and an accountant in the future. ",
+    "description": "Eighteen year old Imai is a secondary student from Bubugo village. She thrives at school and is quick to show compassion to all those around her. Imai loves science and dreams of finishing school so she can become a nun and an accountant in the future.",
     "imageName": require("../assets/images/bssp/Imai2018.jpg")
   },
   {
     "title": "Jackline",
-    "description": "Jackline is 20 and the oldest of 10 children. In addition to helping take care of her siblings, she works hard in school. She hopes to become a hairdresser in the future and is studying hairdressing at YMCA. This kind girl is quick to make friends and is loving to all those around her.",
+    "description": "Jackline is 23 and the oldest of 10 children. She recently graduated from YMCA Vocational School with a certificate in hairdressing. This kind girl is quick to make friends and is loving to all those around her.",
     "imageName": require("../assets/images/bssp/Jackline2016.jpg")
   },
   {
     "title": "Jacob",
-    "description": "Jacob is from Nakakulwe village where his mother, Irene, and three other children live. This ambitious twenty-two-year-old dreams of completing his education and becoming a software engineer. He finished his Computer Science Certificate and is working on his advanced certificate, so he can apply to university.",
+    "description": "Jacob is from Nakakulwe village where his mother, Irene, and three other children live. This ambitious 24-year-old dreams of completing his education and becoming a software engineer. He finished his Computer Science Certificate and is working on his advanced certificate, so he can apply to university.",
     "imageName": require("../assets/images/bssp/Jacob2016.jpg")
   },
   {
@@ -58,62 +58,62 @@ export const Faces: Face[] = [
   },
   {
     "title": "Julius",
-    "description": "Julius is 23 years old and is looked after by his grandmother, Beatrice, who participates in the agriculture program. Julius is studying in an electrical engineering program at Nile Vocational Institute.",
+    "description": "Julius is 23 years old and is looked after by his sweet grandmother, Beatrice, a member of our Nakakulwe village group. Julius is studying in an Electrical Engineering program at Kampala International University.",
     "imageName": require("../assets/images/bssp/Julius.jpg")
   },
   {
     "title": "Christine K.",
-    "description": "Sixteen-year-old Christine is from Bubugo village where her parents and four brothers and sisters live. She loves languages, both English and her local language of Lusoga. Christine is a secondary student who hopes to become a teacher in the future.",
+    "description": "Eighteen-year-old Christine is from Bubugo village where her parents and four brothers and sisters live. She loves languages, both English and her local language of Lusoga. Christine is a secondary student who hopes to become a teacher in the future.",
     "imageName": require("../assets/images/bssp/Christine2018.jpg")
   },
   {
     "title": "Leah",
-    "description": "Leah is originally from Namagera village, where her mother, Betty, and six younger siblings live.  Leah enjoys her education and finished with a Business Management Certificate. Now she’s studying at Makerere University Business School.",
+    "description": "Leah is originally from Namagera village, where her mother, Betty, and six younger siblings live. Leah enjoys her education and is working on a degree from Makere University in Business Administration.",
     "imageName": require("../assets/images/bssp/Leah2016.jpg")
   },
   {
     "title": "Mary",
-    "description": "Mary is fourteen years old and has five brothers and sisters. She is one of the top students in her class, and her favorite subjects are religious education and math. Mary hopes to be a nurse in the future.",
+    "description": "Mary is sixteen years old and has five brothers and sisters. She is one of the top students in her class, and her favorite subject is chemistry because it is “the king of all subjects”. Mary hopes to be a nurse in the future.",
     "imageName": require("../assets/images/bssp/Mary2018.jpg")
   },
   {
     "title": "Gloria",
-    "description": "Seventeen-year-old Gloria is a secondary student from Nakakulwe village. She thrives in school and her best subjects are chemistry and biology. Gloria works very hard on her academics and hopes to be a doctor once she completes her education.",
+    "description": "Twenty-year-old Gloria is an incredibly hard working student from Namagera village. She has graduated from secondary school and hopes to pursue a degree in medicine.",
     "imageName": require("../assets/images/bssp/Gloria2018.jpg")
   },
   {
     "title": "Paul",
-    "description": "Paul is from Nakakulwe village and is in his last year of pursuing a bachelor of science degree in chemical engineering. He is a well rounded student and active at his university. In Paul’s free time he likes to write screenplays and play FIFA.",
+    "description": "Paul is from Nakakulwe village and has finished his coursework for a degree in chemical engineering. He is awaiting his graduation ceremony and is working part-time. In Paul’s free time he likes to write screenplays and play FIFA.",
     "imageName": require("../assets/images/bssp/Paul2016.jpg")
   },
   {
     "title": "Racheal",
-    "description": "Racheal lives with her grandmother and is 1 of 3 kids in her family. She is 21 years old and attends YMCA, where she is studying Institutional Catering. Education is the most important thing to her so she can have a bright future.",
+    "description": "Racheal lives with her grandmother and is 1 of 3 kids in her family. She is 23 years old and is in vocational school working on a certificate in hotel management. Education is the most important thing to her so she can have a bright future.",
     "imageName": require("../assets/images/bssp/Racheal2016.jpg")
   },
   {
     "title": "Resty",
-    "description": "Resty is a primary student in Nakakulwe village. She is taken care of by her mother, Annet, and six brothers and sisters. She does well in school and her favorite subjects are math and English. Resty hopes to complete her education so she can be a teacher in the future.",
+    "description": "Resty is a secondary student in Nakakulwe village. She is taken care of by her mother, Annet, and six brothers and sisters. She does well in school and her favorite subjects are math and English. Resty hopes to complete her education so she can be a teacher in the future.",
     "imageName": require("../assets/images/bssp/Resty2018.jpg")
   },
   {
     "title": "Sandra",
-    "description": "Sandra is 15 years old and her favorite subject is social studies. She hopes to be a doctor some day. There are 7 children in her family, and her favorite foods are potatoes, rice, and chicken. Sandra is sweet, friendly, and always smiling.",
+    "description": "Sandra is 17 years old and her favorite subjects are geography and history. She hopes to be a journalist one day. There are 7 children in her family, and her favorite foods are potatoes, rice, and chicken. Sandra is sweet, friendly, and always smiling.",
     "imageName": require("../assets/images/bssp/Sandra2018.jpg")
   },
   {
     "title": "Viola",
-    "description": "Viola is 14 years old and is one of 6 children in her family. She is from Bubugo village and is cared for by her grandmother. Her favorite subject is math and she hopes to be a nurse. She loves running and her favorite food is potatoes and cabbage.",
+    "description": "Viola is 16 years old and is one of 6 children in her family. She is from Bubugo village and is cared for by her grandmother. Her favorite subject is math and she hopes to be a nurse. She loves running and her favorite food is potatoes and cabbage.",
     "imageName": require("../assets/images/bssp/Viola2018.jpg")
   },
   {
     "title": "Titus",
-    "description": "This little guy, Titus, is 11 years old and is one of 6 children in his family. He attends primary school and for fun he enjoys playing soccer with his friends. He hopes to become a doctor someday, “so that he can help orphaned children” or a pilot.",
+    "description": "This little guy, Titus, is 13 years old and is one of 6 children in his family. He attends secondary school and for fun he enjoys playing soccer with his friends. He hopes to become a pilot one day!",
     "imageName": require("../assets/images/bssp/Titus2018.jpg")
   },
   {
     "title": "Violate",
-    "description": "Violate is seventeen years old. She is taken care of by her mother, Aidah, and has seven brothers and sisters. Violate attends secondary school and she loves learning English and math. Viola aspires to be a nurse or an accountant when she grows up.",
+    "description": "Violate is twenty years old and has seven siblings. She has graduated from secondary school and is attending YMCA Vocational School to pursue a certificate in accounting.",
     "imageName": require("../assets/images/bssp/Violate2018.jpg")
   },
   {
