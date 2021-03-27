4342df09813136da8dc3af8b3e4809a1f1fe926e
Nelson Chen <crazysim@gmail.com>
2021-03-27 16:33:01 -0700

    Clone existing Beads component and name it Meet The Women
    https://trello.com/c/rtLQOdz9/594-create-a-separate-page-for-meet-the-women-in-shopify

---
+++ src/MeetTheWomen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <face-block-grid :faces="faces" :title-description="true"></face-block-grid>
+  </div>
+</template>
+
+<script>
+  import FaceBlockGrid from './components/FaceBlockGrid';
+  import Faces from './data/meet_the_women.ts';
+
+  export default {
+    name: 'meet_the_women',
+    components: { FaceBlockGrid },
+    data() {
+      return {
+        faces: Faces,
+      };
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+  .container {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 62.5rem;
+  }
+</style>
---
+++ src/data/meet_the_women.ts
@@ -0,0 +1,401 @@
+import Face from './face';
+
+export const Faces: Face[] = [
+  {
+    "title": "Akiya Rose",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/akiya.jpg")
+  },
+  {
+    "title": "Alinyinza Aidah",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/alinyinza.jpg")
+  },
+  {
+    "title": "Amoding Jane",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/amoding.jpg")
+  },
+  {
+    "title": "Ayanga Joy",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/ayanga.jpg")
+  },
+  {
+    "title": "Babiryo Scovia",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/babiryo.jpg")
+  },
+  {
+    "title": "Babita Hajjira",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/babita.jpg")
+  },
+  {
+    "title": "Bagume Catherine",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/bagume.jpg")
+  },
+  {
+    "title": "Banada Dominic",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/banada.jpg")
+  },
+  {
+    "title": "Basirika Rose",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/basirika.jpg")
+  },
+  {
+    "title": "Bibirye Nasabu",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/bibirye.jpg")
+  },
+  {
+    "title": "Biryeri Elizabeth",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/biryeri.jpg")
+  },
+  {
+    "title": "Buyinzika Rose",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/buyinzika.jpg")
+  },
+  {
+    "title": "Byakika Mariam",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/byakika.jpg")
+  },
+  {
+    "title": "Byobona Robinah",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/byobona.jpg")
+  },
+  {
+    "title": "Ekesa Rose",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/ekesa.jpg")
+  },
+  {
+    "title": "Kabale Aidah",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/kabale.jpg")
+  },
+  {
+    "title": "Kabeta Margret",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/kabeta.jpg")
+  },
+  {
+    "title": "Kafuko Florence",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/kafuko.jpg")
+  },
+  {
+    "title": "Kafuko Robinah",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/kafukor.jpg")
+  },
+  {
+    "title": "Kagoya Hadijja",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/kagoya.jpg")
+  },
+  {
+    "title": "Kanifa Nali",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/kanifa.jpg")
+  },
+  {
+    "title": "Kaudha Jane",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/kaudha.jpg")
+  },
+  {
+    "title": "Kayanga Irene",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/kayanga.jpg")
+  },
+  {
+    "title": "Kirabira Jane",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/kirabira.jpg")
+  },
+  {
+    "title": "Kirya Lovinsa",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/kirya.jpg")
+  },
+  {
+    "title": "Kizire Tabitha",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/kizire.jpg")
+  },
+  {
+    "title": "Logose Rukiah",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/logose.jpg")
+  },
+  {
+    "title": "Magoba Margret",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/magoba.jpg")
+  },
+  {
+    "title": "Mukyala Elizabeth",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/mukyala.jpg")
+  },
+  {
+    "title": "Mutesi Lovinsa",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/mutesi.jpg")
+  },
+  {
+    "title": "Mutesi Zulaika",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/mutesiz.jpg")
+  },
+  {
+    "title": "Muwanguzi Ruth",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/muwanguzi.jpg")
+  },
+  {
+    "title": "Mwagale Zewulensi",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/mwagale.jpg")
+  },
+  {
+    "title": "Nabirye Ednasi",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nabirye.jpg")
+  },
+  {
+    "title": "Nabirye Annet",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nabiryea.jpg")
+  },
+  {
+    "title": "Nabirye Robinah",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nabiryer.jpg")
+  },
+  {
+    "title": "Naigaga Tereza",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/naigaga.jpg")
+  },
+  {
+    "title": "Naigaga Rose",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/naigagar.jpg")
+  },
+  {
+    "title": "Naisanga Zabina",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/naisanga.jpg")
+  },
+  {
+    "title": "Naisanga Irene",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/naisangai.jpg")
+  },
+  {
+    "title": "Nakafeero Joyce",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/nakafeero.jpg")
+  },
+  {
+    "title": "Nakaibale Halima",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/nakaibale.jpg")
+  },
+  {
+    "title": "Nakaibale Annet",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nakaibalea.jpg")
+  },
+  {
+    "title": "Nakaliti Ruth",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/nakaliti.jpg")
+  },
+  {
+    "title": "Nakyewe Sylivia",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nakyewe.jpg")
+  },
+  {
+    "title": "Nalongo Justine",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/nalongo.jpg")
+  },
+  {
+    "title": "Namajja Nasimu",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/namajja.jpg")
+  },
+  {
+    "title": "Namalili Jane",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/namalili.jpg")
+  },
+  {
+    "title": "Nambi Betty",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/nambi.jpg")
+  },
+  {
+    "title": "Namboira Florence",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/namboira.jpg")
+  },
+  {
+    "title": "Nampeera Loy",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nampeera.jpg")
+  },
+  {
+    "title": "Namugabo Betty",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/namugabo.jpg")
+  },
+  {
+    "title": "Namugabo Annet",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/namugaboa.jpg")
+  },
+  {
+    "title": "Namugabo Scovia",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/namugabos.jpg")
+  },
+  {
+    "title": "Namukose Jane",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/namukose.jpg")
+  },
+  {
+    "title": "Namukuta Elizabeth",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/namukuta.jpg")
+  },
+  {
+    "title": "Namulimba Ruth",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/namulimbar.jpg")
+  },
+  {
+    "title": "Namukuyi Jesca",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/namukuyi.jpg")
+  },
+  {
+    "title": "Namulinda Aidah",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/namulinda.jpg")
+  },
+  {
+    "title": "Namuwaya Rose",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/namuwaya.jpg")
+  },
+  {
+    "title": "Nanagwe Elizabeth",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nanagwe.jpg")
+  },
+  {
+    "title": "Nangobi Rebecca",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/nangobi.jpg")
+  },
+  {
+    "title": "Nangobi Mastula",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nangobim.jpg")
+  },
+  {
+    "title": "Nankya Esther",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/nankya.jpg")
+  },
+  {
+    "title": "Nantono Mary",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/nantono.jpg")
+  },
+  {
+    "title": "Ndhaye Mariam",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/ndhaye.jpg")
+  },
+  {
+    "title": "Ndibogeza Sarah",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/ndibogeza.jpg")
+  },
+  {
+    "title": "Ndiwayoki Florence",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/ndiwayoki.jpg")
+  },
+  {
+    "title": "Nkwewembye Fauza",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/nkwewembye.jpg")
+  },
+  {
+    "title": "Nuume Catherine",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/nuume.jpg")
+  },
+  {
+    "title": "Nyende Aidah",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/nyende.jpg")
+  },
+  {
+    "title": "Olooka Sabastaino",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/olooka.jpg")
+  },
+  {
+    "title": "Semakula Jesca",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/semakula.jpg")
+  },
+  {
+    "title": "Sewaguma Rashid",
+    "description": "Bubugo Village",
+    "imageName": require("../assets/images/meet_the_women/sewaguma.jpg")
+  },
+  {
+    "title": "Tabingwa Matilda",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/tabingwa.jpg")
+  },
+  {
+    "title": "Tibitondwa Beatrice",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/tibitondwa.jpg")
+  },
+  {
+    "title": "Tulyanabo Aidah",
+    "description": "Nakakulwe Village",
+    "imageName": require("../assets/images/meet_the_women/tulyanabo.jpg")
+  },
+  {
+    "title": "Tusubira Betty",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/tusubira.jpg")
+  },
+  {
+    "title": "Ziraba Mary",
+    "description": "Namagera Village",
+    "imageName": require("../assets/images/meet_the_women/ziraba.jpg")
+  }
+];
+
+export default Faces;
