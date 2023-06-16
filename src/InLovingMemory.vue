72e61b9000fadb3da7cabe900291d903dd8fcc96
Nelson Chen <crazysim@gmail.com>
2023-06-16 08:49:13 -0700

    1st pass of "In Loving Memory"

---
+++ src/InLovingMemory.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <face-block-grid :faces="faces" :title-description="true"></face-block-grid>
+  </div>
+</template>
+
+<script>
+  import FaceBlockGrid from './components/FaceBlockGrid';
+  import Faces from './data/in_loving_memory.ts';
+
+  export default {
+    name: 'in_loving_memory',
+    components: { FaceBlockGrid },
+    data() {
+      return {
+        faces: Faces,
+      };
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+  .container {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 62.5rem;
+  }
+</style>
---
+++ src/data/in_loving_memory.ts
@@ -0,0 +1,11 @@
+import Face from './face';
+
+export const Faces: Face[] = [
+  {
+    "title": "Derick",
+    "description": "Derick was a university student from Bubugo village studying motor vehicle engineering. Sadly, in 2021 he died unexpectedly. He is deeply missed and he will forever be remembered in BSSP.",
+    "imageName": require("../assets/images/in_loving_memory/Derick2018.jpg")
+  },
+];
+
+export default Faces;
