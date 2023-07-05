3b3e91b794a360ebe30e6035c5b7c393f05e1308
Nelson Chen <crazysim@gmail.com>
2023-07-05 16:38:16 -0700

    died -> passed away
    https://trello.com/c/QqOnqK5W/613-update-dericks-bio

--- src/data/in_loving_memory.ts
+++ src/data/in_loving_memory.ts
@@ -3,7 +3,7 @@ import Face from './face';
 export const Faces: Face[] = [
   {
     "title": "Derick",
-    "description": "Derick was a university student from Bubugo village studying motor vehicle engineering. Sadly, in 2021 he died unexpectedly. He is deeply missed and he will forever be remembered in BSSP.",
+    "description": "Derick was a university student from Bubugo village studying motor vehicle engineering. Sadly, in 2021 he passed away unexpectedly. He is deeply missed and he will forever be remembered in BSSP.",
     "imageName": require("../assets/images/in_loving_memory/Derick2018.jpg")
   },
 ];
